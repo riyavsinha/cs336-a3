94d169475f840c9cd05f7c6fd55f9a1cfb695ea5
Riya Sinha <riya.v.sinha@gmail.com>
2026-05-06 14:23:46 -0700

    update plots

--- cs336_scaling/hw/isoflops_laws.py
+++ cs336_scaling/hw/isoflops_laws.py
@@ -14,12 +14,7 @@ def plot_loss_curves(df: pd.DataFrame, save_path: str):
   plt.figure()
   for cb, group in df.groupby("compute_budget"):
     group = group.sort_values("parameters")
-    plt.plot(
-      group["parameters"],
-      group["final_loss"],
-      marker="o",
-      label=f"{cb:.0e}"
-    )
+    plt.plot(group["parameters"], group["final_loss"], marker="o", label=f"{cb:.0e}")
   plt.xlabel("Parameters")
   plt.ylabel("Loss")
   plt.xscale("log")
@@ -27,20 +22,19 @@ def plot_loss_curves(df: pd.DataFrame, save_path: str):
   plt.grid(True)
   plt.savefig(save_path)
   
-def plot_against_compute(df: pd.DataFrame, y:str, ylabel: str, save_path: str):
+def plot_against_compute(df: pd.DataFrame, pred_df: pd.DataFrame, y:str, ylabel: str, save_path: str):
   os.makedirs(os.path.dirname(save_path), exist_ok=True)
   df = df.sort_values("compute_budget")
+  pred_df = pred_df.sort_values("compute_budget")
   plt.figure()
-  plt.plot(
-    df["compute_budget"],
-    df[y],
-    marker="o",
-  )
+  plt.plot(pred_df["compute_budget"], pred_df[y], color="tab:orange", marker="s", label="predicted")
+  plt.scatter(df["compute_budget"], df[y], color="tab:blue", label="observed", zorder=100)
   plt.xlabel("Compute Budget")
   plt.ylabel(ylabel)
   plt.xscale("log")
   plt.title(f"{ylabel} vs Compute Budget")
   plt.grid(True)
+  plt.legend()
   plt.savefig(save_path)
 
 def power_law(C, a, b):
@@ -70,14 +64,14 @@ if __name__ == "__main__":
   
   # a)
   min_df, fit_fn = fit_params(df)
-  preds = pd.DataFrame({"compute_budget": cbs, "parameters": [fit_fn(cb) for cb in cbs]})
-  pred_df = pd.concat([min_df, preds])
+  pred_cbs = sorted(set(cbs) | set(min_df["compute_budget"]))
+  preds = pd.DataFrame({"compute_budget": pred_cbs, "parameters": [fit_fn(cb) for cb in pred_cbs]})
   print(preds.to_latex(index=False, float_format="%.3e"))
-  plot_against_compute(pred_df, "parameters", "Parameters", "figs/2a_params.png")
+  plot_against_compute(min_df, preds, "parameters", "Parameters", "figs/2a_params.png")
   
   # b)
   min_d_df, fit_d_fn = fit_data(df)
-  preds = pd.DataFrame({"compute_budget": cbs, "d": [fit_d_fn(cb) for cb in cbs]})
-  pred_df = pd.concat([min_d_df, preds])
+  pred_cbs = sorted(set(cbs) | set(min_d_df["compute_budget"]))
+  preds = pd.DataFrame({"compute_budget": pred_cbs, "d": [fit_d_fn(cb) for cb in pred_cbs]})
   print(preds.to_latex(index=False, float_format="%.3e"))
-  plot_against_compute(pred_df, "d", "Data Size", "figs/2b_data.png")
+  plot_against_compute(min_d_df, preds, "d", "Data Size", "figs/2b_data.png")
